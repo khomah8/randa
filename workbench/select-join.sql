a809d6c64e0fabf0bef535275019a41347361c76
khomah8 <59750654+khomah8@users.noreply.github.com>
2020-12-19 00:12:36 +0200

    Update select-join.sql

--- workbench/select-join.sql
+++ workbench/select-join.sql
@@ -1,2 +1,11 @@
--- two tables   
-SELECT SUM(Players.Balance) FROM Players JOIN UserAgents ON Players.UserAgent = UserAgents.UserAgent WHERE ((Players.PlayerName LIKE ‘QA%’) AND (UserAgents.Browser=’Chrome’)); 
+-- two Tables data set 
+
+-- simple condition select 
+-- Write an SQL query that will show all the PlayerID of all players with more than 50 in there balance :
+SELECT PlayerID FROM Players WHERE Balance>50;
+
+-- several conditions select 
+-- Write an SQL query that will summarize the balance for all Chrome players whose name start with "QA" :
+SELECT SUM(Players.Balance) FROM Players 
+JOIN UserAgents ON Players.UserAgent = UserAgents.UserAgent 
+WHERE ((Players.PlayerName LIKE ‘QA%’) AND (UserAgents.Browser=’Chrome’)); 
