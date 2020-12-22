a4e1cde4813f7a16c433e8722b13ed03308ff6bc
khomah8 <59750654+khomah8@users.noreply.github.com>
2020-12-23 00:35:26 +0200

    Create trysql.sql

---
+++ trysql.sql
@@ -0,0 +1,14 @@
+-- sql INLINE comment 
+
+/* sql 
+	multiline comment
+*/
+
+CREATE TABLE Persons
+(
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);
