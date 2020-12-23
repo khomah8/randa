7c6b30d83e9f8ae4d4bd59ec7faeefc3495f4abc
khomah8 <59750654+khomah8@users.noreply.github.com>
2020-12-23 18:25:36 +0200

    Update sql-stored-procedures.sql

--- workbench/sql-stored-procedures.sql
+++ workbench/sql-stored-procedures.sql
@@ -1,6 +1,9 @@
 -- from https://www.w3schools.com/sql/sql_stored_procedures.asp 
-
-`CREATE PROCEDURE SelectAllCustomers @City nvarchar(30), @PostalCode nvarchar(10)
+-- define proc 
+CREATE PROCEDURE SelectAllCustomers @City nvarchar(30), @PostalCode nvarchar(10)
 AS
 SELECT * FROM Customers WHERE City = @City AND PostalCode = @PostalCode
-GO;`
+GO;
+
+-- then exec with params 
+EXEC SelectAllCustomers @City = 'London', @PostalCode = 'WA1 1DP';
