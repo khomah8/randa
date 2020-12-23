32450cc091940f4f652ce163a22a339cbe37e59d
khomah8 <59750654+khomah8@users.noreply.github.com>
2020-12-23 18:43:27 +0200

    Update data-between-tables.sql

--- workbench/data-between-tables.sql
+++ workbench/data-between-tables.sql
@@ -19,6 +19,10 @@ WHERE 1 = 0;
 
 -- read details on  https://www.w3schools.com/sql/sql_select_into.asp 
 
+/* The INSERT INTO SELECT statement copies data from one table and inserts it into another table.
+  - INSERT INTO SELECT requires that data types in source and target tables match
+  - The existing records in the target table are unaffected
+*/
 
 INSERT INTO table2 (column1, column2, column3, ...)
 SELECT column1, column2, column3, ...
@@ -26,4 +30,7 @@ FROM table1
 WHERE condition;
 
 -- read on  https://www.w3schools.com/sql/sql_insert_into_select.asp
--- try on  https://www.w3schools.com/sql/trysql.asp?filename=trysql_insert_into_select 
+/*  try on  
+  https://www.w3schools.com/sql/trysql.asp?filename=trysql_insert_into_select 
+  https://www.w3schools.com/sql/trysql.asp?filename=trysql_insert_into_select2
+*/ 
