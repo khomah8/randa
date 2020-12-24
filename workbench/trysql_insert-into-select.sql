a963065fdf59448906d8cc65c431abd6ac4c70ad
khomah8 <59750654+khomah8@users.noreply.github.com>
2020-12-24 18:07:47 +0200

    Update trysql_insert-into-select.sql

--- workbench/trysql_insert-into-select.sql
+++ workbench/trysql_insert-into-select.sql
@@ -4,9 +4,18 @@
 SELECT Suppliers.supplierName, Customers.CustomerName
 FROM Suppliers
 INNER JOIN Customers ON Suppliers.supplierName = Customers.CustomerName;
+/*
+Number of Records: 32
+*/ 
 
--- Number of Records: 32
-
--- then do Inserting 
+-- 2) then do Inserting 
 INSERT INTO Customers (CustomerName, City, Country)
 SELECT SupplierName, City, Country FROM Suppliers;
+
+-- 3) resulting comparision 
+SELECT Suppliers.Country as supplCountry, Suppliers.City as supplCity, Suppliers.supplierName, Customers.CustomerName, Customers.Country as custCountry, Customers.City as custCity, Customers.PostalCode, CustomerID  
+FROM Suppliers 
+JOIN Customers ON Suppliers.supplierName = Customers.CustomerName ORDER BY CustomerID;
+/*
+Number of Records: 61
+*/
